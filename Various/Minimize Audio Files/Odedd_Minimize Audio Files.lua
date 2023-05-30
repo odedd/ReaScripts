bcc476f159fa4e3766befa486ce2eb9430436d60
Oded Davidov <davidovoded@gmail.com>
2023-05-30 12:25:04 +0300

    AttachFont instead of Attach

--- Various/Minimize Audio Files/Odedd_Minimize Audio Files.lua
+++ Various/Minimize Audio Files/Odedd_Minimize Audio Files.lua
@@ -35,11 +35,17 @@ dofile(p .. 'lib/Minimize.lua')
 dofile(p .. 'lib/Gui.lua')
 dofile(p .. 'lib/App.lua')
 
+gui.tables = {
+    horizontal = {
+        flags1 = r.ImGui_TableFlags_ScrollX() | r.ImGui_TableFlags_ScrollY() | r.ImGui_TableFlags_BordersOuter() |
+            r.ImGui_TableFlags_Borders() | r.ImGui_TableFlags_NoHostExtendX() | r.ImGui_TableFlags_SizingFixedFit()
+    }
+}
+
 if OD_PrereqsOK({
     reaimgui_version = '0.7',
     sws = true
 }) then
-
 
     local function doPerform()
         r.Undo_BeginBlock()
@@ -58,13 +64,11 @@ if OD_PrereqsOK({
     function checkPerform()
         if app.coPerform then
             if coroutine.status(app.coPerform) == "suspended" then
-                
-                reaper.ShowConsoleMsg(tablelength(app.mediaFiles)..'\n')
-        
+
                 coroutine.resume(app.coPerform)
                 retval, app.perform.status = coroutine.resume(app.coPerform)
                 if not retval then
-                   r.ShowConsoleMsg(app.perform.status)
+                    r.ShowConsoleMsg(app.perform.status)
                 end
             elseif coroutine.status(app.coPerform) == "dead" then
                 app.coPerform = nil
@@ -73,10 +77,11 @@ if OD_PrereqsOK({
     end
 
     function app.drawPerform(open)
+        local bottom_lines = 2
         if open then
             local ctx = gui.ctx
-            r.ImGui_SetNextWindowSize(ctx, 700,
-                math.min(1000, select(2, r.ImGui_Viewport_GetSize(r.ImGui_GetMainViewport(ctx)))), r.ImGui_Cond_Appearing())
+            r.ImGui_SetNextWindowSize(ctx, 700, math.min(1000, select(2, r.ImGui_Viewport_GetSize(
+                r.ImGui_GetMainViewport(ctx)))), r.ImGui_Cond_Appearing())
             r.ImGui_SetNextWindowPos(ctx, 100, 100, r.ImGui_Cond_FirstUseEver())
             local visible, open = r.ImGui_Begin(ctx, scr.name .. ' v' .. scr.version .. "##performWindow", true)
             gui.mainWindow = {
@@ -84,9 +89,30 @@ if OD_PrereqsOK({
                 size = {r.ImGui_GetWindowSize(ctx)}
             }
             if visible then
-                for filename, filenameinfo in pairsByOrder(app.mediaFiles) do
-                    reaper.ImGui_Text(ctx,string.format("%02d",filenameinfo.order)..' '..filenameinfo.basename..' ('..tablelength(filenameinfo.occurrences)..')')
+                local childHeight = select(2, r.ImGui_GetContentRegionAvail(ctx)) -
+                                        (r.ImGui_GetFrameHeightWithSpacing(ctx) * bottom_lines +
+                                            r.ImGui_GetStyleVar(ctx, r.ImGui_StyleVar_ItemSpacing()) * 2)
+                -- if r.ImGui_CollapsingHeader(ctx,"Stem Selection",false,r.ImGui_TreeNodeFlags_DefaultOpen()) then
+                if r.ImGui_BeginChild(ctx, 'mediaFiles', 0, childHeight) then
+                    if r.ImGui_BeginTable(ctx, 'table_scrollx', 2, gui.tables.horizontal.flags1) then
+                        --- SETUP MATRIX TABLE
+                        local parent_open, depth, open_depth = true, 0, 0
+                        r.ImGui_TableSetupScrollFreeze(ctx, 1, 2)
+                        r.ImGui_TableSetupColumn(ctx, 'File', r.ImGui_TableColumnFlags_NoHide(), nil) -- Make the first column not hideable to match our use of TableSetupScrollFreeze()
+
+                        r.ImGui_TableSetupColumn(ctx, 'Occurances', nil, nil)
+                        for filename, filenameinfo in pairsByOrder(app.mediaFiles) do
+                            r.ImGui_TableNextRow(ctx)
+                            r.ImGui_TableNextColumn(ctx)
+                            r.ImGui_Text(ctx, filenameinfo.basename)
+                            r.ImGui_TableNextColumn(ctx)
+                            r.ImGui_Text(ctx, #filenameinfo.occurrences)
+                        end
+                        r.ImGui_EndTable(ctx)
+                    end
+                    r.ImGui_EndChild(ctx)
                 end
+
                 r.ImGui_End(ctx)
             end
             return open
@@ -109,8 +135,7 @@ if OD_PrereqsOK({
             r.ImGui_PopStyleColor(ctx)
         end
         if not app.coPerform then
-            if r.ImGui_Button(ctx, 'Minimize Files',
-                r.ImGui_GetContentRegionAvail(ctx)) then
+            if r.ImGui_Button(ctx, 'Minimize Files', r.ImGui_GetContentRegionAvail(ctx)) then
                 saveSettings()
                 app.coPerform = coroutine.create(doPerform)
             end
@@ -143,13 +168,19 @@ if OD_PrereqsOK({
                 r.ImGui_BeginDisabled(ctx)
             end
 
-            settings.suffix = gui.setting('text', 'suffix',
-                "Suffix to be added to minimized files", settings.suffix)
-            settings.padding = gui.setting('dragdouble', 'padding (s)',
-            "How much audio to before and after items", settings.padding, {speed=0.1,min=0.0,max=10.0, format="%.1f"})
-            if settings.padding < 0 then settings.padding = 0 end
+            settings.suffix = gui.setting('text', 'suffix', "Suffix to be added to minimized files", settings.suffix)
+            settings.padding = gui.setting('dragdouble', 'padding (s)', "How much audio to before and after items",
+                settings.padding, {
+                    speed = 0.1,
+                    min = 0.0,
+                    max = 10.0,
+                    format = "%.1f"
+                })
+            if settings.padding < 0 then
+                settings.padding = 0
+            end
 
---            app.drawMatrices(ctx, bottom_lines)
+            --            app.drawMatrices(ctx, bottom_lines)
             if app.coPerform and coroutine.status(app.coPerform) == 'running' then
                 r.ImGui_EndDisabled(ctx)
             end
