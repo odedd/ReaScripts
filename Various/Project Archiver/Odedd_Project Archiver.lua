653814d21c6ff95841992e8ee1d33fd51bf728b8
Oded Davidov <davidovoded@gmail.com>
2023-06-19 15:07:29 +0300

    updated thread link

--- Various/Project Archiver/Odedd_Project Archiver.lua
+++ Various/Project Archiver/Odedd_Project Archiver.lua
@@ -1,8 +1,8 @@
 -- @description Project Archiver
 -- @author Oded Davidov
--- @version 0.5.5
+-- @version 0.5.6
 -- @donation https://paypal.me/odedda
--- @link Forum Thread https://forum.cockos.com/showthread.php?p=2686126
+-- @link Forum Thread https://forum.cockos.com/showthread.php?t=280150
 -- @license GNU GPL v3
 -- @provides
 --   [nomain] ../../Resources/Common/* > Resources/Common/
