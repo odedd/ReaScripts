2988bebe256235b1dbe8da2eaa38ee683041c010
Oded Davidov <davidovoded@gmail.com>
2023-06-26 22:02:23 +0300

    settings error fix

--- Various/Project Archiver/Odedd_Project Archiver.lua
+++ Various/Project Archiver/Odedd_Project Archiver.lua
@@ -1,6 +1,6 @@
 -- @description Project Archiver
 -- @author Oded Davidov
--- @version 0.6.4
+-- @version 0.6.5
 -- @donation https://paypal.me/odedda
 -- @link Forum Thread https://forum.cockos.com/showthread.php?t=280150
 -- @license GNU GPL v3
@@ -13,8 +13,7 @@
 --   [nomain] ../../Resources/Icons/* > Resources/Icons/
 --   [nomain] lib/**
 -- @changelog
--- @changelog
---   Internal changes
+--   Settings fix
 
 ---------------------------------------
 -- SETUP ------------------------------
