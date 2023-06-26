804918b9d96df4ef9edd9acf000ec89863a39e91
Oded Davidov <davidovoded@gmail.com>
2023-06-26 14:13:41 +0300

    Updated Strings

--- Resources/Common/Helpers/Strings.lua
+++ Resources/Common/Helpers/Strings.lua
@@ -12,16 +12,27 @@ function OD_EscapePattern(text)
     return text:gsub("([^%w])", "%%%1")
 end
 
+function OD_MagicFix(str)
+  return str:gsub("[%(%)%.%+%-%*%?%[%]%^%$%%]", "%%%1")
+end
+
 function OD_CaseInsensitivePattern(pattern)
 
-    -- find an optional '%' (group 1) followed by any character (group 2)
-    local p = pattern:gsub("(%%?)(.)", function(percent, letter)
   
-      if percent ~= "" or not letter:match("%a") then
+    -- find an optional '%' (group 1) followed by any character (group 2)
+    local classStarted = false
+    local p = pattern:gsub("(%%?)(.)", function(percent, letter)
+      
+      if percent ~= "" or not letter:match("%a") or classStarted then
+        if letter == '[' then 
+          classStarted = true
+        elseif letter == ']' then
+          classStarted = false
+        end
         -- if the '%' matched, or `letter` is not a letter, return "as is"
         return percent .. letter
-      else
-        -- else, return a case-insensitive character class of the matched letter
+      elseif not classStarted then
+        -- else, return a case-insensitive character class of the matched letter 
         return string.format("[%s%s]", letter:lower(), letter:upper())
       end
   
