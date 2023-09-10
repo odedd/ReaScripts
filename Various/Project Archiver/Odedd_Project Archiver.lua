d4bd475c68760d60fd22ef89909d2d77478575a8
Oded Davidov <davidovoded@gmail.com>
2023-09-10 11:52:19 +0300

    changelog & version bump

--- Various/Project Archiver/Odedd_Project Archiver.lua
+++ Various/Project Archiver/Odedd_Project Archiver.lua
@@ -1,6 +1,6 @@
 -- @description Project Archiver
 -- @author Oded Davidov
--- @version 0.7.6
+-- @version 0.7.7
 -- @donation https://paypal.me/odedda
 -- @link Forum Thread https://forum.cockos.com/showthread.php?t=280150
 -- @license GNU GPL v3
@@ -13,7 +13,7 @@
 --   [nomain] ../../Resources/Icons/* > Resources/Icons/
 --   [nomain] lib/**
 -- @changelog
---   Ignore click items
+--   Lus 5.4 (Reaper 7) compatibility
 
 ---------------------------------------
 -- SETUP ------------------------------
