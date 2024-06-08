5365408b000839885bf930ff854b8839ae5930ba
Oded Davidov <davidovoded@gmail.com>
2024-06-08 12:50:09 +0300

    version bump

--- FX/Send Buddy/Odedd_Send Buddy.lua
+++ FX/Send Buddy/Odedd_Send Buddy.lua
@@ -1,6 +1,6 @@
 -- @description Send Buddy
 -- @author Oded Davidov
--- @version 1.0.12
+-- @version 1.0.13
 -- @donation https://paypal.me/odedda
 -- @license GNU GPL v3
 -- @about
@@ -22,7 +22,7 @@
 --   [nomain] ../../Resources/Icons/* > Resources/Icons/
 --   [nomain] lib/**
 -- @changelog
---   Basic Logging added
+--   Deeper Logging
 
 ---------------------------------------
 -- SETUP ------------------------------
