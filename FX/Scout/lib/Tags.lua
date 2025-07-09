84d06147c19e1ed51581e17b22d81bfe646c405c
Oded Davidov <davidovoded@gmail.com>
2025-07-09 09:29:29 +0300

    tag color change

--- FX/Scout/lib/Tags.lua
+++ FX/Scout/lib/Tags.lua
@@ -4,13 +4,13 @@ PB_Tags = OD_Settings:new({
         -- Defineable in GUI
         favorites = {},
         tagInfo ={
-            [1]={name='EQ', color=0x32a852ff, synonyms={'Equaliser'}, order = 1},
-            [2]={name='Compressor', color=0x8a3cbaff, parentId=4, order = 2},
-            [3]={name='Spectral Processor', color=0xb94bbdff, synonyms={'Resonance Surpressor'}, parentId=1, order = 1},
-            [4]={name='Dynamics', color=0x3255a8ff, order = 2},
-            [5]={name='Gate', color=0x65248cff, parentId=4, order = 1},
-            [6]={name='Instruments', color=0xf5e042ff, order = 3},
-            [7]={name='Multiband', color=0xe6bc9aff, parentId=2, order = 1}},
+            [1]={name='EQ', color=0x242429ff, synonyms={'Equaliser'}, order = 1},
+            [2]={name='Compressor', color=0x242429ff, parentId=4, order = 2},
+            [3]={name='Spectral Processor', color=0x242429ff, synonyms={'Resonance Surpressor'}, parentId=1, order = 1},
+            [4]={name='Dynamics', color=0x242429ff, order = 2},
+            [5]={name='Gate', color=0x242429ff, parentId=4, order = 1},
+            [6]={name='Instruments', color=0x242429ff, order = 3},
+            [7]={name='Multiband', color=0x242429ff, parentId=2, order = 1}},
         taggedAssets = {
             ["1 /Library/Audio/Plug-Ins/VST3/FabFilter Pro-Q 4.vst3"] = {7,3},
             ["1 /Library/Audio/Plug-Ins/VST3/FabFilter Pro-Q 3.vst3"] = {1},
