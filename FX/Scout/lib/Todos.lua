034dcdd3029cbd04651aa807e0c32fd381ae951c
Oded Davidov <davidovoded@gmail.com>
2025-07-21 20:13:15 +0300

    updated todo

--- FX/Scout/lib/Todos.lua
+++ FX/Scout/lib/Todos.lua
@@ -9,7 +9,6 @@
 -- TODO: Create actions for presets/filters
 -- TODO: Settings window
 -- TODO: Fix Hints
--- TODO: Allow favorite multiple results
 -- TODO: Multiple results via mouse (?)
 -- TODO: Reset filter button
 -- TODO: Better visual representation of search mode
