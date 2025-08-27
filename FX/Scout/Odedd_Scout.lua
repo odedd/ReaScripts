f5a004e3d7e788e58a0184d2f43ac41c0a494135
Oded Davidov <davidovoded@gmail.com>
2025-08-27 18:21:40 +0300

    Update Scout (v1.0.3)

--- FX/Scout/Odedd_Scout.lua
+++ FX/Scout/Odedd_Scout.lua
@@ -1,6 +1,6 @@
 -- @description Scout
 -- @author Oded Davidov
--- @version 1.0.2
+-- @version 1.0.3
 -- @donation https://paypal.me/odedda
 -- @link Product Page https://www.random.tools/l/scout-plus?utm_source=scout&utm_medium=script&utm_campaign=reapack
 -- @license GNU GPL v3
