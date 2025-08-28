a7da5de6962af85b7dcb44be2e3aaee984a3ccb1
Oded Davidov <davidovoded@gmail.com>
2025-08-28 20:52:30 +0300

    Update Scout (v1.0.5)

--- FX/Scout/Odedd_Scout.lua
+++ FX/Scout/Odedd_Scout.lua
@@ -1,6 +1,6 @@
 -- @description Scout
 -- @author Oded Davidov
--- @version 1.0.4
+-- @version 1.0.5
 -- @donation https://paypal.me/odedda
 -- @link Product Page https://www.random.tools/l/scout-plus?utm_source=scout&utm_medium=script&utm_campaign=reapack
 -- @license GNU GPL v3
@@ -17,7 +17,11 @@
 --   [nomain] Resources/Fonts/**
 --   [nomain] scout54.dat
 -- @changelog
---   Fixed edge case where enter is ignored
+--   Fixed: Right clicking in filter search mode causes crash (no menu though)
+--   Fixed: Using random result shortcut produces two random results
+--   Fixed: After action: clear text search setting does not get saved after restart + hint is nil
+--   Option/Alt clicking tag + or - buttons or filter clears filter
+
 r = reaper
 DATA = _VERSION == 'Lua 5.4' and 'scout54'
 if DATA == nil then
