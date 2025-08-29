b89276c5b01eeb7e4b2fb8f52283fa385bcea287
Oded Davidov <davidovoded@gmail.com>
2025-08-29 09:08:36 +0300

    Update Scout (v1.0.9)

--- FX/Scout/Odedd_Scout.lua
+++ FX/Scout/Odedd_Scout.lua
@@ -1,6 +1,6 @@
 -- @description Scout
 -- @author Oded Davidov
--- @version 1.0.8
+-- @version 1.0.9
 -- @donation https://paypal.me/odedda
 -- @link Product Page https://www.random.tools/l/scout-plus?utm_source=scout&utm_medium=script&utm_campaign=reapack
 -- @license GNU GPL v3
@@ -17,8 +17,7 @@
 --   [nomain] Resources/Fonts/**
 --   [nomain] scout54.dat
 -- @changelog
---   Instruments/FX separated
---   Added setting to select newly added tracks
+--   User configurable highlight/background colors
 
 r = reaper
 DATA = _VERSION == 'Lua 5.4' and 'scout54'
