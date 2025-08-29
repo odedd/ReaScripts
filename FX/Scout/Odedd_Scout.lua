82900e5f21e300686a6d0eb05854ec83159edfd7
Oded Davidov <davidovoded@gmail.com>
2025-08-29 09:22:22 +0300

    Update Scout (v1.0.11)

--- FX/Scout/Odedd_Scout.lua
+++ FX/Scout/Odedd_Scout.lua
@@ -1,6 +1,6 @@
 -- @description Scout
 -- @author Oded Davidov
--- @version 1.0.10
+-- @version 1.0.11
 -- @donation https://paypal.me/odedda
 -- @link Product Page https://www.random.tools/l/scout-plus?utm_source=scout&utm_medium=script&utm_campaign=reapack
 -- @license GNU GPL v3
@@ -17,7 +17,7 @@
 --   [nomain] Resources/Fonts/**
 --   [nomain] scout54.dat
 -- @changelog
---   Color fix
+--   Small wording change in settings window
 
 r = reaper
 DATA = _VERSION == 'Lua 5.4' and 'scout54'
