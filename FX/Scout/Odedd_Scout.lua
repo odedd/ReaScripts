08d4534142a8e9d60e6b66721fd236f51bd9f73e
Oded Davidov <davidovoded@gmail.com>
2025-08-29 13:19:42 +0300

    Update Scout (v1.0.12)

--- FX/Scout/Odedd_Scout.lua
+++ FX/Scout/Odedd_Scout.lua
@@ -1,6 +1,6 @@
 -- @description Scout
 -- @author Oded Davidov
--- @version 1.0.11
+-- @version 1.0.12
 -- @donation https://paypal.me/odedda
 -- @link Product Page https://www.random.tools/l/scout-plus?utm_source=scout&utm_medium=script&utm_campaign=reapack
 -- @license GNU GPL v3
@@ -17,7 +17,8 @@
 --   [nomain] Resources/Fonts/**
 --   [nomain] scout54.dat
 -- @changelog
---   Small wording change in settings window
+--   Cmd/Ctrl+Click creates a new track with FX/FX Chain on it
+--   Instrument tracks now get named by the instrument name (if "Use instrument tracks" setting is checked)
 
 r = reaper
 DATA = _VERSION == 'Lua 5.4' and 'scout54'
