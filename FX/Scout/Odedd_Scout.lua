611d4902e2e0da2542da9a6b9c3f465ee34d58d2
Oded Davidov <davidovoded@gmail.com>
2025-08-31 21:09:27 +0300

    Update Scout (v1.0.22)

--- FX/Scout/Odedd_Scout.lua
+++ FX/Scout/Odedd_Scout.lua
@@ -1,6 +1,6 @@
 -- @description Scout
 -- @author Oded Davidov
--- @version 1.0.21
+-- @version 1.0.22
 -- @donation https://paypal.me/odedda
 -- @link Product Page https://www.random.tools/l/scout-plus?utm_source=scout&utm_medium=script&utm_campaign=reapack
 -- @license GNU GPL v3
@@ -17,7 +17,7 @@
 --   [nomain] Resources/Fonts/**
 --   [nomain] scout54.dat
 -- @changelog
---   Fix result highlighting when switching search modes
+--   Added setting to activate magic words with either tab or space
 
 r = reaper
 DATA = _VERSION == 'Lua 5.4' and 'scout54'
