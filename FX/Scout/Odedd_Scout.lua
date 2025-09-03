a0b0d2ad6d382d0a27acb7a818343ef150e85e0f
Oded Davidov <davidovoded@gmail.com>
2025-09-03 17:41:26 +0300

    Update Scout (v1.0.25)

--- FX/Scout/Odedd_Scout.lua
+++ FX/Scout/Odedd_Scout.lua
@@ -1,6 +1,6 @@
 -- @description Scout
 -- @author Oded Davidov
--- @version 1.0.24
+-- @version 1.0.25
 -- @donation https://paypal.me/odedda
 -- @link Product Page https://www.random.tools/l/scout-plus?utm_source=scout&utm_medium=script&utm_campaign=reapack
 -- @license GNU GPL v3
@@ -17,8 +17,7 @@
 --   [nomain] Resources/Fonts/**
 --   [nomain] scout54.dat
 -- @changelog
---   New asset type: Project FX
---   Fuzzy Search optimized
+--   Setting to turn off grouping by FX format
 
 r = reaper
 DATA = _VERSION == 'Lua 5.4' and 'scout54'
