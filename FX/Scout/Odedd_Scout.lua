a11618c18dac9b71f46451be7d9a70aeb17d5a08
Oded Davidov <davidovoded@gmail.com>
2025-09-04 14:27:34 +0300

    Update Scout (v1.0.29)

--- FX/Scout/Odedd_Scout.lua
+++ FX/Scout/Odedd_Scout.lua
@@ -1,6 +1,6 @@
 -- @description Scout
 -- @author Oded Davidov
--- @version 1.0.28
+-- @version 1.0.29
 -- @donation https://paypal.me/odedda
 -- @link Product Page https://www.random.tools/l/scout-plus?utm_source=scout&utm_medium=script&utm_campaign=reapack
 -- @license GNU GPL v3
@@ -17,7 +17,7 @@
 --   [nomain] Resources/Fonts/**
 --   [nomain] scout54.dat
 -- @changelog
---   Fixed bug that prevented things to be added to recents (introduced in v1.0.26)
+--   Can now open multiple Projects/Project templates
 
 r = reaper
 DATA = _VERSION == 'Lua 5.4' and 'scout54'
