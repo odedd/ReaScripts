c1fb62aafb953375f6ceb4520c4ca967e0559547
Oded Davidov <davidovoded@gmail.com>
2025-09-10 12:57:11 +0300

    Update Scout (v1.0.33)

--- FX/Scout/Odedd_Scout.lua
+++ FX/Scout/Odedd_Scout.lua
@@ -1,6 +1,6 @@
 -- @description Scout
 -- @author Oded Davidov
--- @version 1.0.32
+-- @version 1.0.33
 -- @donation https://paypal.me/odedda
 -- @link Product Page https://www.random.tools/l/scout-plus?utm_source=scout&utm_medium=script&utm_campaign=reapack
 -- @license GNU GPL v3
@@ -17,7 +17,8 @@
 --   [nomain] Resources/Fonts/**
 --   [nomain] scout54.dat
 -- @changelog
---   Reset QuickChain preset on load if set to reset filter
+--   Fix results not launching with Enter (introduced in 1.0.31)
+--   Added Keypad Enter to launch results
 
 r = reaper
 DATA = _VERSION == 'Lua 5.4' and 'scout54'
