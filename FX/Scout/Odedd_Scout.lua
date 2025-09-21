0158ee29c4a7ddbd358b1ad2baddfc98c22ecb7a
Oded Davidov <davidovoded@gmail.com>
2025-09-21 08:19:04 +0200

    Update Scout (v1.1.8)

--- FX/Scout/Odedd_Scout.lua
+++ FX/Scout/Odedd_Scout.lua
@@ -1,6 +1,6 @@
 -- @description Scout
 -- @author Oded Davidov
--- @version 1.1.7
+-- @version 1.1.8
 -- @donation https://paypal.me/odedda
 -- @link Product Page https://www.random.tools/l/scout-plus?utm_source=scout&utm_medium=script&utm_campaign=reapack
 -- @license GNU GPL v3
@@ -18,13 +18,7 @@
 --   [nomain] scout54.dat
 --   [main] Odedd_Scout - Start in sleep mode.lua
 -- @changelog
---   New setting - Clear search text when changing between Item and Filter search modes
---   New setting - Include group name in search
---   Ability to combine presets (secret option - to enable toggle 'allowCombinePresets' in the Odedd_Scout.ini file to true when scout is not running (NOT in sleep mode)) 
---   QuickTag window - allow selection with numpad enter 
---   Close QuickChain panel on wakeup if "reset filters on wakeup" is checked 
---   Ability to unhide hidden tags even if their parents are hidden (unless all tags are hidden by the "hide all tags" button)
---   New tag list context item - "Move to..."
+--   Move to - prevent moving tag into itself + allow moving to root level
 r = reaper
 DATA = _VERSION == 'Lua 5.4' and 'scout54'
 if DATA == nil then
