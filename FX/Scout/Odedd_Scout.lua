65cf3d7724017d4c941e2c0110790e05c6922f28
Oded Davidov <davidovoded@gmail.com>
2025-09-25 08:29:55 +0200

    Update Scout (v1.1.11)

--- FX/Scout/Odedd_Scout.lua
+++ FX/Scout/Odedd_Scout.lua
@@ -1,6 +1,6 @@
 -- @description Scout
 -- @author Oded Davidov
--- @version 1.1.10
+-- @version 1.1.11
 -- @donation https://paypal.me/odedda
 -- @link Product Page https://www.random.tools/l/scout-plus?utm_source=scout&utm_medium=script&utm_campaign=reapack
 -- @license GNU GPL v3
@@ -18,7 +18,8 @@
 --   [nomain] scout54.dat
 --   [main] Odedd_Scout - Start in sleep mode.lua
 -- @changelog
---   Fix - loading presets using magic words
+--   Tags drag and drop (filter search mode -> tag list)
+--   Fix - variant order not respected
 r = reaper
 DATA = _VERSION == 'Lua 5.4' and 'scout54'
 if DATA == nil then
