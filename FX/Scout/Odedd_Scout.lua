a4a38d2ca63e1bae8cb5154b3cc1f4382020d0b5
Oded Davidov <davidovoded@gmail.com>
2025-09-29 17:35:10 +0300

    Update Scout (v1.1.14)

--- FX/Scout/Odedd_Scout.lua
+++ FX/Scout/Odedd_Scout.lua
@@ -1,6 +1,6 @@
 -- @description Scout
 -- @author Oded Davidov
--- @version 1.1.13
+-- @version 1.1.14
 -- @donation https://paypal.me/odedda
 -- @link Product Page https://www.random.tools/l/scout-plus?utm_source=scout&utm_medium=script&utm_campaign=reapack
 -- @license GNU GPL v3
@@ -18,7 +18,8 @@
 --   [nomain] scout54.dat
 --   [main] Odedd_Scout - Start in sleep mode.lua
 -- @changelog
---   Result highlighting in QuickTag window and in "Move to..." context menu
+--   Fixed: When clear search on mode change is OFF, pressing Enter to run a result doesn't reset the search string.
+--   Fixed: When Include group name in search is ON, results in the recents tab are not searchable by their "actual" category.
 r = reaper
 DATA = _VERSION == 'Lua 5.4' and 'scout54'
 if DATA == nil then
