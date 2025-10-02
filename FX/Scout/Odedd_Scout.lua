693ee4125066c34e8ebcad71a90344eba8231f52
Oded Davidov <davidovoded@gmail.com>
2025-10-02 07:10:53 +0300

    Update Scout (v1.1.16)

--- FX/Scout/Odedd_Scout.lua
+++ FX/Scout/Odedd_Scout.lua
@@ -1,6 +1,6 @@
 -- @description Scout
 -- @author Oded Davidov
--- @version 1.1.15
+-- @version 1.1.16
 -- @donation https://paypal.me/odedda
 -- @link Product Page https://www.random.tools/l/scout-plus?utm_source=scout&utm_medium=script&utm_campaign=reapack
 -- @license GNU GPL v3
@@ -18,7 +18,7 @@
 --   [nomain] scout54.dat
 --   [main] Odedd_Scout - Start in sleep mode.lua
 -- @changelog
---   New setting: Chain window on multiple FX
+--   Added license information in bytecode file
 r = reaper
 DATA = _VERSION == 'Lua 5.4' and 'scout54'
 if DATA == nil then
